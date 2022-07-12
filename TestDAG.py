0a514778d50b7c08cb7e4eb8ef2814c6be088bde
Irving FGR <irving.fgr@gmail.com>
2022-07-12 18:26:53 -0500

    Modified testdag

--- TestDAG.py
+++ TestDAG.py
@@ -1,15 +1,17 @@
-from datetime import datetime
-from airflow import DAG
+#from datetime import datetime
+#from airflow import DAG
+#from airflow.operators.dummy_operator import DummyOperator
+#from airflow.operators.python_operator import PythonOperator
+
+from airflow.models import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
 
-def print_test():
-    return 'Test DAG in Airflow DAG!'
+with DAG("db_ingestion", start_date=days_ago(1)) as dag:
+    start_workflow = DummyOperator(task_id="start_workflow")
+    validate = DummyOperator(task_id="validate")
+    prepare = DummyOperator(task_id="prepare")
+    load = DummyOperator(task_id="load")
+    end_workflow = DummyOperator(task_id="end_workflow")
 
-dag = DAG('test_dag', description='Test DAG',
-          schedule_interval='0 12 * * *',
-          start_date=datetime(2017, 3, 20), catchup=False)
-
-test_operator = PythonOperator(task_id='test_task', python_callable=print_test, dag=dag)
-
-test_operator
+    start_workflow >> validate >> prepare >> load >> end_workflow
