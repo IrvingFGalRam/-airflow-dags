db94dca5201a1c900e09c673299554416921d7e3
Irving FGR <irving.fgr@gmail.com>
2022-07-14 20:03:26 -0500

    Add prepare functionality to test postfre conn

--- TestDAG.py
+++ TestDAG.py
@@ -5,13 +5,9 @@
 
 from airflow.models import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.utils.dates import days_ago
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.operators.python import PythonOperator
-
-
-def ingest_data(
-    hook = PostgresHook(postgres_conn_id="ml_conn")
+from airflow.utils.dates import days_ago
+#from airflow.operators.python import PythonOperator
     
 
 with DAG(
@@ -21,22 +17,8 @@ with DAG(
     validate = DummyOperator(task_id="validate")
     prepare = PostgresOperator(
         task_id="prepare",
-        postgres_conn_id="ml_conn",
-        sql=f"""
-            CREATE TABLE IF NOT EXISTS monthly_charts_data (
-                month VARCHAR(10),
-                position INTEGER,
-                artist VARCHAR(100),
-                song VARCHAR(100),
-                indicative_revenue NUMERIC,
-                us INTEGER,
-                uk INTEGER,
-                de INTEGER,
-                fr INTEGER,
-                ca INTEGER,
-                au INTEGER
-            )
-        """,
+        postgres_conn_id=postgres_default,
+        sql="",
     )
     load = DummyOperator(task_id="load")
     end_workflow = DummyOperator(task_id="end_workflow")
