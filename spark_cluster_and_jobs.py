93f14d860e51535a8a80fd0b612198ddbc8f4ad4
Irving FGR <irving.fgr@gmail.com>
2022-08-06 19:03:05 -0500

    Modified spark_cluster DAG to try submiting spark hobs into ephimeral dataproc clusters

---
+++ spark_cluster_and_jobs.py
@@ -0,0 +1,168 @@
+"""Template DAG
+Author: Irving FGR
+Description: Ingests the data from a GCS bucket into a postgres table.
+"""
+
+import os
+from datetime import datetime
+from airflow.models import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.google.cloud.operators.dataproc import (
+    DataprocCreateClusterOperator,
+    DataprocDeleteClusterOperator,
+    DataprocSubmitJobOperator,
+)
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.operators.python import PythonOperator
+from airflow.operators.sql import BranchSQLOperator
+from airflow.providers.google.cloud.hooks.gcs import GCSHook
+from airflow.providers.google.cloud.sensors.gcs import GCSObjectExistenceSensor
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.utils.dates import days_ago
+
+# General constants
+DAG_ID = "dataproc_spark"
+PROJECT_ID = os.environ.get("SYSTEM_TESTS_GCP_PROJECT", "")
+CLUSTER_NAME = "cluster-dataproc-hive-deb"
+CLOUD_PROVIDER = "gcp"
+STABILITY_STATE = "unstable"
+REGION = "us-central1"
+ZONE = "us-central1-a"
+
+# GCP constants
+GCP_CONN_ID = "google_cloud_default"
+GCS_BUCKET_NAME = "capstone-project-wzl-storage"
+# GCS_KEY_NAME = "noheader_test_log_reviews.csv"
+
+# Postgres constants
+POSTGRES_CONN_ID = "postgres_default"
+POSTGRES_TABLE_NAME = "user_purchase"
+
+CLUSTER_CONFIG = {
+    "master_config": {
+        "num_instances": 1,
+        "machine_type_uri": "n1-standard-2",
+        "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 50},
+    },
+    "worker_config": {
+        "num_instances": 2,
+        "machine_type_uri": "n1-standard-2",
+        "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 50},
+
+    },
+    "softwareConfig": {
+        "image_version": "2.0",
+        "properties": {
+            "spark.jars.packages": "com.databricks:spark-xml_2.12:0.13.0,org.apache.spark:spark-mllib_2.12:3.1.3,org.apache.spark:spark-avro_2.12:3.1.3"
+        }
+    }
+}
+
+TIMEOUT = {"seconds": 1 * 2 * 60 * 60}
+
+# Jobs definitions
+# [START how_to_cloud_dataproc_spark_config]
+SPARK_JOB = {
+    "reference": {"project_id": PROJECT_ID},
+    "placement": {"cluster_name": CLUSTER_NAME},
+    "spark_job": {
+        "jar_file_uris": ["file:///usr/lib/spark/examples/jars/spark-examples.jar"],
+        "main_class": "org.apache.spark.examples.SparkPi",
+
+    },
+}
+SPARK_JOB_TEST = {
+    "reference": {"project_id": PROJECT_ID},
+    "placement": {"cluster_name": CLUSTER_NAME},
+    "spark_job": {
+        "jar_file_uris": ["gs://capstone-project-wzl-storage/jars/scala-jobs_2.12-0.1.1.jar"],
+        "main_class": "org.example.TestSparkSession",
+
+    },
+}
+# SPARK_JOB_SHOW_TABLE = {
+#     "reference": {"project_id": PROJECT_ID},
+#     "placement": {"cluster_name": CLUSTER_NAME},
+#     "spark_job": {
+#         "jar_file_uris": ["gs://capstone-project-wzl-storage/jars/scala-jobs_2.12-0.1.1.jar"],
+#         "main_class": "org.example.TestSparkSession",
+#     },
+#     "arguments": {
+#         "gs://capstone-project-wzl-storage/silver/classified_movie_review",
+#         "avro",
+#         "25"
+#     }
+# }
+SPARK_JOB_T_CMR = {
+    "reference": {"project_id": PROJECT_ID},
+    "placement": {"cluster_name": CLUSTER_NAME},
+    "spark_job": {
+        "jar_file_uris": ["gs://capstone-project-wzl-storage/jars/scala-jobs_2.12-0.1.1.jar"],
+        "main_class": "org.example.TransformClassifiedMovieReview ",
+    },
+}
+SPARK_JOB_T_RL = {
+    "reference": {"project_id": PROJECT_ID},
+    "placement": {"cluster_name": CLUSTER_NAME},
+    "spark_job": {
+        "jar_file_uris": ["gs://capstone-project-wzl-storage/jars/scala-jobs_2.12-0.1.1.jar"],
+        "main_class": "org.example.TransformReviewLogs ",
+    },
+}
+SPARK_JOB_T_UP = {
+    "reference": {"project_id": PROJECT_ID},
+    "placement": {"cluster_name": CLUSTER_NAME},
+    "spark_job": {
+        "jar_file_uris": ["gs://capstone-project-wzl-storage/jars/scala-jobs_2.12-0.1.1.jar"],
+        "main_class": "org.example.TransformUserPurchase ",
+    },
+}
+
+
+with DAG(
+    dag_id=DAG_ID,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    tags=[CLOUD_PROVIDER, "dataproc"],
+) as dag:
+    start_workflow = DummyOperator(task_id="start_workflow")
+
+    create_cluster = DataprocCreateClusterOperator(
+        task_id="create_cluster",
+        project_id=PROJECT_ID,
+        cluster_config=CLUSTER_CONFIG,
+        region=REGION,
+        cluster_name=CLUSTER_NAME,
+
+    )
+
+    spark_task = DataprocSubmitJobOperator(
+        task_id="spark_task",
+        job=SPARK_JOB_T_CMR,
+        region=REGION,
+        project_id=PROJECT_ID
+    )
+
+    delete_cluster = DataprocDeleteClusterOperator(
+        task_id="delete_cluster",
+        project_id=PROJECT_ID,
+        cluster_name=CLUSTER_NAME,
+        region=REGION,
+        trigger_rule=TriggerRule.ALL_DONE,
+    )
+
+    end_workflow = DummyOperator(task_id="end_workflow")
+
+    start_workflow >> create_cluster >> spark_task >> delete_cluster >> end_workflow
+
+    from tests.system.utils.watcher import watcher
+
+    # This test needs watcher in order to properly mark success/failure
+    # when "teardown" task with trigger rule is part of the DAG
+    list(dag.tasks) >> watcher()
+
+from tests.system.utils import get_test_run  # noqa: E402
+
+# Needed to run the example DAG with pytest (see: tests/system/README.md#run_via_pytest)
+[docs]test_run = get_test_run(dag)
